10435987a40f1ece636d6da9aca0388f83d63e10
zyrex12 <149414677+zyrex12@users.noreply.github.com>
2025-05-30 18:47:23 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: { serverActions: true },
 };
 module.exports = nextConfig;
-
-// ======== vercel.json ========
-{
-  "version": 2,
-  "builds": [
-    { "src": "next.config.js", "use": "@vercel/next" }
-  ]
-}
